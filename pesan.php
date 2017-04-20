6fe072085d1aa69c55b66c68ba4d25bd256d2411
karang24 <tunggal.jaka@gmail.com>
2017-04-21 06:12:43 +0700

    Delete pesan.php

--- pesan.php
+++
@@ -1,7 +0,0 @@
-<p>Anda mendapatkan email dari sistem</p>
-<ul>
-    <li>Nama Lengkap : {{ $data->nama_lengkap }}</li>
-    <li>Tanggal Lahir : {{$data->tgl_lahir}}</li>
-    <li>Username : {{$data->username}}</li>
-    <li>Password : ************* </li>
-</ul>
