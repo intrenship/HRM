8032a709db7616aa85c39c2856f17589dce80f9e
farizarianu12 <farizarianu@gmail.com>
2017-06-11 09:43:41 +0700

    fz

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,12 +23,21 @@ Route::group(['middleware' => ['web','auth']], function(){
 	Route::get('/superadmin', 'HomeController@superadmin');
 	//news
 	Route::get('/add_news', 'adminController@create_news');
+	Route::get('/add_daylireport', 'adminController@create_daylireport');
 	Route::post('/store', 'adminController@insert_news');
 	Route::get('/view_news', 'adminController@views_news');
 	Route::get('/edit_news/{id}', 'adminController@edit_news');
 	Route::post('/update_news/{id}', 'adminController@update_news');
 	Route::get('/delete/{id}', 'adminController@destroy');
 	Route::get('/query_news', 'adminController@search_news');
+	Route::get('/query_karyawan', 'adminController@search_report');
+	Route::get('/add_karyawan', 'adminController@create_karaywan');
+	Route::get('/edit_karyawan/{id}', 'adminController@edit_karyawan');
+	Route::get('/view_karyawan', 'adminController@views_karyawan');
+	Route::get('/view_report_karyawan', 'adminController@report_karyawan');
+	Route::get('/delete_karyawan/{id}', 'adminController@destroy_karyawan');
+	Route::post('/karyawan_insert', 'adminController@insert_karyawan');
+	Route::post('/daylireport_insert', 'adminController@insert_daylireport');
 
 	//users
 	Route::get('/add_users', 'adminController@create_users');
@@ -39,11 +48,13 @@ Route::group(['middleware' => ['web','auth']], function(){
 	Route::get('/test', 'userController@biodata');
 	Route::get('/add_biodata/{id}', 'adminController@biodata');
 	Route::get('/edit_biodata/{id}', 'adminController@editbiodata');
+	Route::get('/edit_lamaran/{id}', 'adminController@edit_lamaran');
 	Route::post('/update_biodata/{id_user}', 'adminController@updatebiodata');
 	Route::post('/lamaran_kerja/', 'adminController@lamaran');
 	Route::get('/view_users', 'adminController@views_users');
 	Route::get('/edit_users/{id}', 'adminController@edit_users');
-	Route::post('/update_userens/{id}', 'adminController@update_users');
+	Route::post('/update_users/{id}', 'adminController@update_users');
+	Route::post('/update_lamaran/{id}', 'adminController@update_lamaran');
 	Route::get('/query_users', 'adminController@search_user');
 	Route::post('/users_insert', 'adminController@register1');
 	Route::get('/delete_user/{id}', 'adminController@destroy_user');
@@ -53,6 +64,8 @@ Route::group(['middleware' => ['web','auth']], function(){
 	Route::post('/insert_biodata3', 'adminController@insert_biodata3');
 	
 	Route::post('/lamaran', 'adminController@insert_lamaran');
+	Route::get('/viewlamaran/{id}', 'adminController@viewlamaran1');
+	Route::get('/kirim_email/{id}', 'MailController@html_email');
 	Route::post('/lamaran1', 'adminController@insert_lamaran1');
 	Route::get('/lamaran3', 'adminController@lamaran_view');
 
@@ -68,7 +81,9 @@ Route::group(['middleware' => ['web','auth']], function(){
 	Route::get('/search_lowongan/{id}', 'adminController@cari_lowongan');
 	Route::get('/search_lowongan1/', 'adminController@cari_lowongan1');
 	//biodata//
-
+Route::get('sendbasicemail','MailController@postmakeInsuranceClaim') ;
+Route::get('sendhtmlemail','MailController@html_email') ;
+Route::get('sendattachmentemail','MailController@attachment_email') ;
 	
 	Route::get('/login1', function(){
 		if (Auth::user()->id_group == 1){
